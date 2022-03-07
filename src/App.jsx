50c7d699a6b234216f2120dda0273581d06553e3
Julian Bleecker <bleeckerj@gmail.com>
2022-03-07 11:12:42 -0800

    WTF?

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import Install from './components/Install';
 import Home from './components/Home';
 
 function App() {
-  return <Home />;
-  // if (window.ethereum) {
-  //   return <Home />;
-  // } else {
-  //   return <Install />
-  // }
+  if (window.ethereum) {
+    return <Home />;
+  } else {
+    return <Install />
+  }
 }
 
 export default App;
